a86faf20b8ec2039f0fcfc7afd51928c9c8a178c
maxiscript777 <max.amestica@duocuc.cl>
2026-05-10 01:35:24 -0400

    Proyecto Reservas

---
+++ src/main/java/com/example/PaginaSpotIa/PaginaSpotIaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PaginaSpotIa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaSpotIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaSpotIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PaginaSpotIa/PaginaSpotIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PaginaSpotIa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaSpotIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
