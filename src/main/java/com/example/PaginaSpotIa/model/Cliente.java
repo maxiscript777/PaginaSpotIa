2dc39bdc279a769973edda1bade1a83c0e56f476
maxiscript777 <max.amestica@duocuc.cl>
2026-05-10 01:53:08 -0400

    aa

--- src/main/java/com/example/PaginaSpotIa/model/Cliente.java
+++ src/main/java/com/example/PaginaSpotIa/model/Cliente.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
