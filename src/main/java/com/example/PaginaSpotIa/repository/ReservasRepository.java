34fb37e1598d95a6d7505b91b23229d0e31bc870
FernandoNavarreteDuoc <fer.navarretea@duocuc.cl>
2026-06-01 18:27:10 -0400

    se agrega el @repository

--- src/main/java/com/example/PaginaSpotIa/repository/ReservasRepository.java
+++ src/main/java/com/example/PaginaSpotIa/repository/ReservasRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.PaginaSpotIa.model.Reservas;
 
+@Repository
 public interface ReservasRepository
         extends JpaRepository<Reservas, Integer> {
 
